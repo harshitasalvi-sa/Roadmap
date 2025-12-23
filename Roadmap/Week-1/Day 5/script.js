e738650b9ba631b355753889be5875bd48200141
harshitasalvi-sa <harshita.salvi@startappss.com>
2025-12-23 18:43:40 +0530

    Started counter app

---
+++ Roadmap/Week-1/Day 5/script.js
@@ -0,0 +1,15 @@
+let incr_btn = document.getElementById('incr-btn');
+
+let decr_btn = document.getElementById('decr-btn');
+
+function counter(){
+    let count = 0;
+
+    function increment(){
+        return 
+    }
+}
+
+incr_btn.addEventListener("click", ()=>{
+
+})
