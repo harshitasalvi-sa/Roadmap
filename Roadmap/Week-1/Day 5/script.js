6d65a852598ec62587713ea0017e07d704430683
harshitasalvi-sa <harshita.salvi@startappss.com>
2025-12-24 11:47:30 +0530

    Completed Counter app

--- Roadmap/Week-1/Day 5/script.js
+++ Roadmap/Week-1/Day 5/script.js
@@ -1,15 +1,45 @@
-let incr_btn = document.getElementById('incr-btn');
 
-let decr_btn = document.getElementById('decr-btn');
 
-function counter(){
+function counter(min, max){
     let count = 0;
 
-    function increment(){
-        return 
+    let incrBtn = document.getElementById('incr-btn');
+    let decrBtn = document.getElementById('decr-btn');
+    let countSpan = document.getElementById('count');
+    let msg = document.getElementById('msg');
+
+    function render(){
+        countSpan.innerText = `${count}`;
     }
+
+    function increment(){
+        if(count < max){
+            //msg.innerText = "";
+            msg.style.display = "none";
+            count++;
+        }
+        else{
+            msg.style.display = "block";
+            msg.innerText = "Maximum count reached!";
+        }
+        render();
+    }
+
+    function decrement(){
+        if(count > min){
+            msg.style.display = "none";
+            count--;
+        }
+        else{
+            msg.style.display = "block";
+            msg.innerText = "Minimum count reached!"
+        }
+        render();
+    }
+
+    incrBtn.addEventListener("click", increment);
+    decrBtn.addEventListener("click", decrement);
 }
 
-incr_btn.addEventListener("click", ()=>{
+counter(-10, 4);
 
-})
