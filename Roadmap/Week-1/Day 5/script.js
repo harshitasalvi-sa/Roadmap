3ab0117db895890caebb38d81b21181357c6cad2
harshitasalvi-sa <harshita.salvi@startappss.com>
2025-12-30 12:36:46 +0530

    Created calulator app

--- Roadmap/Week-1/Day 5/script.js
+++ Roadmap/Week-1/Day 5/script.js
@@ -3,6 +3,9 @@
 function counter(min, max){
     let count = 0;
 
+    let prev1 = 0;
+    let prev2 = 1;
+
     let incrBtn = document.getElementById('incr-btn');
     let decrBtn = document.getElementById('decr-btn');
     let countSpan = document.getElementById('count');
@@ -12,10 +15,24 @@ function counter(min, max){
         countSpan.innerText = `${count}`;
     }
 
+    // function incrementFib(){
+    //     count = prev1+prev2;
+    //     prev1 = prev2;
+    //     prev2 = count;
+    // }
+     // function decrementFib(){
+    //     count -= (prev1+prev2);
+    //     prev1 = prev2;
+    //     prev2 = count;
+    // }
+
     function increment(){
         if(count < max){
             msg.style.display = "none";
-            count++;
+            //count++;
+            count = prev1+prev2;
+            prev1 = prev2;
+            prev2 = count;
         }
         else{
             msg.style.display = "block";
@@ -28,7 +45,10 @@ function counter(min, max){
     function decrement(){
         if(count > min){
             msg.style.display = "none";
-            count--;
+            //count--;
+            count -= prev2;
+            prev2 = prev1;
+            //1 1 2 3 5 8 
         }
         else{
             msg.style.display = "block";
--- Roadmap/Week-2/Day-1/script.js
+++ Roadmap/Week-2/Day-1/script.js
@@ -1,5 +1,90 @@
+let numBtns = document.querySelectorAll(".num");
+let operatorBtns = document.querySelectorAll(".operator");
+let equalBtns = document.getElementById("equal");
+let clearBtn = document.getElementById("clear")
+let display = document.getElementById("display");
 
-const btn = document.getElementsByClassName("num");
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+
+let string = "";
+
+
+clearBtn.addEventListener("click", ()=>{
+    display.innerText = "";
+    firstNumber = "";
+    secondNumber = "";
+    operator = "";
+})
+
+numBtns.forEach((btn) =>{
+    btn.addEventListener("click", ()=>{
+        if(operator === ""){
+            firstNumber += btn.innerText;
+            display.innerText = firstNumber;
+        }
+        else{
+            secondNumber += btn.innerText;
+            // string = secondNumber;
+            display.innerText += secondNumber;
+        }
+    })
+})
+
+operatorBtns.forEach((button)=>{
+    button.addEventListener("click", ()=>{
+        operator = button.innerText;
+        display.innerText += operator;
+    })
+})
+
+equalBtns.addEventListener("click", ()=>{
+    let num1 = Number(firstNumber);
+    let num2 = Number(secondNumber);
+    let result;
+
+    if(isNaN(num2)){
+        display.innerText = num1;
+        return;
+    }
+
+    if(isNaN(num1) || isNaN(num2) || operator === "") {
+        display.innerText = "Error";
+        return;
+    }
+
+    switch(operator){
+        case "+":
+            result = num1 + num2;
+            break;
+        case "-":
+            result = num1 - num2;
+            break;
+        case "*":
+            result = num1 * num2;
+            break;
+        case "/":
+            if(num2 === 0){
+                display.innerText = "Error";
+                return;
+            }
+            result = num1 / num2;
+            break;
+        case "%":
+            result = num2 % num2;
+            break;
+        default:
+            display.innerText = "Error";
+            return;
+    }
+
+    display.innerText = result;
+
+    firstNumber = result.toString();
+    secondNumber = "";
+    operator = "";
+});
 
 
 function add(a, b){
@@ -17,6 +102,10 @@ function divide(a, b){
     }
     return a/b;
 }
+
+
+
+
 
 
 
