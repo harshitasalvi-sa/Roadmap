8eec2bfed2b2d0d6a6778d069437e97e60bda7df
harshitasalvi-sa <harshita.salvi@startappss.com>
2026-01-03 17:21:54 +0530

    Added keyboard functionality

--- Roadmap/Week-2/Day-1/script.js
+++ Roadmap/Week-2/Day-1/script.js
@@ -1,45 +1,68 @@
-let numBtns = document.querySelectorAll(".num");
+// let numBtns = document.querySelectorAll(".num-pad");
+let numBtns = document.getElementById("num-pad");
 let operatorBtns = document.querySelectorAll(".operator");
 let equalBtns = document.getElementById("equal");
 let clearBtn = document.getElementById("clear")
+let backspaceBtn = document.getElementById("backspaceBtn");
 let display = document.getElementById("display");
 
 let firstNumber = "";
 let secondNumber = "";
 let operator = "";
 
-let string = "";
-
-
-clearBtn.addEventListener("click", ()=>{
-    display.innerText = "";
-    firstNumber = "";
-    secondNumber = "";
-    operator = "";
-})
-
-numBtns.forEach((btn) =>{
-    btn.addEventListener("click", ()=>{
+//delegation
+numBtns.addEventListener("click", (e)=>{
+    if(e.target.tagName === "BUTTON" && e.target.classList.contains("num")){
         if(operator === ""){
-            firstNumber += btn.innerText;
+            firstNumber += e.target.innerText;
             display.innerText = firstNumber;
         }
         else{
-            secondNumber += btn.innerText;
-            // string = secondNumber;
-            display.innerText += secondNumber;
+            secondNumber += e.target.innerText;
+            display.innerText += e.target.innerText;
         }
-    })
-})
+    }
+});
+
+// numBtns.forEach((btn) =>{
+//     btn.addEventListener("click", ()=>{
+//         if(operator === ""){
+//             firstNumber += btn.innerText;
+//             display.innerText = firstNumber;
+//         }
+//         else{
+//             secondNumber += btn.innerText;
+//             display.innerText += btn.innerText;
+//         }
+//     })
+// })
+
 
 operatorBtns.forEach((button)=>{
     button.addEventListener("click", ()=>{
-        operator = button.innerText;
+        
+
+        if (firstNumber !== "" && secondNumber !== "") {
+            calculateResult();
+        }
+        operator = button.innerText; //
         display.innerText += operator;
     })
 })
 
+clearBtn.addEventListener("click", ()=>{
+    handleClear();
+})
+
+backspaceBtn.addEventListener("click", () => {
+    handleBackspace();
+});
+
 equalBtns.addEventListener("click", ()=>{
+    calculateResult();
+});
+
+function calculateResult(){
     let num1 = Number(firstNumber);
     let num2 = Number(secondNumber);
     let result;
@@ -56,23 +79,24 @@ equalBtns.addEventListener("click", ()=>{
 
     switch(operator){
         case "+":
-            result = num1 + num2;
+            //result = num1 + num2;
+            result = add(num1, num2);
             break;
         case "-":
-            result = num1 - num2;
+            //result = num1 - num2;
+            result = substract(num1, num2);
             break;
         case "*":
-            result = num1 * num2;
+            //result = num1 * num2;
+            result = multiply(num1, num2);
             break;
         case "/":
             if(num2 === 0){
                 display.innerText = "Error";
                 return;
             }
-            result = num1 / num2;
-            break;
-        case "%":
-            result = num2 % num2;
+            //result = num1 / num2;
+            result = divide(num1, num2);
             break;
         default:
             display.innerText = "Error";
@@ -84,8 +108,7 @@ equalBtns.addEventListener("click", ()=>{
     firstNumber = result.toString();
     secondNumber = "";
     operator = "";
-});
-
+}
 
 function add(a, b){
     return a+b;
@@ -102,6 +125,62 @@ function divide(a, b){
     }
     return a/b;
 }
+
+//KEYBOARD input
+document.addEventListener("keydown", (e)=>{
+    const key = e.key;
+
+    if(!isNaN(key)){
+        handleNumbers(key);
+    }
+    else if(key === "+" || key === "*" || key === "-" || key === "/"){
+        handleOperators(key);
+    }
+    else if(key === "=" || key === "Enter"){
+        calculateResult();
+    }
+    else if(key === "Backspace"){
+        handleBackspace();
+    }
+    else if(key === "Escape"){
+        handleClear();
+    }
+
+})
+
+function handleNumbers(value){
+    if(operator === ""){
+            firstNumber += value;
+            display.innerText = firstNumber;
+        }
+    else{
+            secondNumber += value;
+            display.innerText += value;
+    }
+}
+
+function handleOperators(op){
+    operator = op;
+    display.innerText += operator;
+}
+
+function handleBackspace(){
+    if (operator === "") {
+        firstNumber = firstNumber.slice(0, -1);
+        display.innerText = firstNumber;
+    } else {
+        secondNumber = secondNumber.slice(0, -1);
+        display.innerText = firstNumber + operator + secondNumber;
+    }
+}
+
+function handleClear(){
+    display.innerText = "";
+    firstNumber = "";
+    secondNumber = "";
+    operator = "";
+}
+
 
 
 
